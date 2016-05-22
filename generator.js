2183e6c33d0abe396e8b3f2d2783c6df62998225
D. C. Rockwell <dcautomates@gmail.com>
2016-05-22 17:02:26 -0600

    Initial commit

---
+++ generator.js
@@ -0,0 +1,30 @@
+import inflect from "jargon";
+
+export default class StimpakReadme {
+	constructor(stimpak) {
+		stimpak
+			.source("**/*")
+				.directory(`${__dirname}/templates`)
+			.prompt({
+				type: "input",
+				name: "projectName",
+				message: "What is the project name?",
+				default: "example-name"
+			}, {
+				type: "input",
+				name: "projectDescription",
+				message: "How would you describe the project?",
+				default: "I don't really know how to describe it!"
+			})
+			.then((stimpak, done) => {
+				const answers = stimpak.answers();
+				const projectNameTitleCase = inflect(answers.projectName).title.toString();
+
+				stimpak.answers({
+					projectNameTitleCase
+				});
+
+				done();
+			});
+	}
+}
---
+++ templates/README.md
@@ -0,0 +1,10 @@
+# <%= projectNameTitleCase %>
+
+<%= projectDescription %>
+
+## Getting Started
+
+```shell
+$ npm install stimpak-<%= projectName %> -g
+$ stimpak <%= projectName %>
+```
